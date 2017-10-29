59a6ecf6f18eab08b006b9fb04e6d07afb5da443
reszlergabor <reszlerg@gmail.com>
2017-10-29 18:02:50 +0100

    * button, mouseover events * ngfor practice * arrays example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'gyakorlas';
+  myArray = [
+    'hihi',
+    'hoho',
+    'pepe',
+    'tritty',
+    'trutty'
+  ];
+  array2 = [33, 6, 9, 11, 15];
+  sum = this.array2.reduce((x, y) => x + y);
+
+  x2() {
+    // console.log(this.array2.map((param: number) => param * 2));
+    this.array2 = (this.array2.map((param: number) => param * 2));
+  }
+
+  xhalf() {
+    // console.log(this.array2.map((param: number) => param * 2));
+    this.array2 = (this.array2.map((param: number) => param * 0.5));
+  }
+
+
 }
